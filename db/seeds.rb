4c72d25dea893426f068c06c7a728de542c75056
lcmaheu <lcmaheu@gmail.com>
2017-10-06 17:38:08 +0000

    seed file for sample data created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+   Blog.create!(
+        title: "My Blog Post #{blog}",
+        body: "Eat and than sleep on your face you call this cat food, 
+            but meow go back to sleep owner brings food and water tries 
+            to pet on head, so scratch get sprayed by water because bad cat, 
+            annoy owner until he gives you food say meow repeatedly 
+            until belly rubs, feels good but the dog smells 
+            bad my slave human didn't give me any food so i 
+            pooped on the floor and hiss at vacuum cleaner. 
+            Friends are not food sniff other cat's 
+            butt and hang jaw half open thereafter for lay 
+            on arms while you're using the keyboard yet massacre 
+            a bird in the living room and then look like the cutest 
+            and most innocent animal on the planet."
+    )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+   Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15 
+    ) 
+end
+
+puts "5 skills create"
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}", 
+        subtitle: "My superb service", 
+        body: "The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg!", 
+        main_image: "http://placehold.it/600x400", 
+        thumb_image: "http://placehold.it/350x200"
+        
+    )
+end
+
+puts "9 portfolio items created"
