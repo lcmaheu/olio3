f5129ef60edaf62c83d0d6794d4b73298d0d0d1f
lcmaheu <lcmaheu@gmail.com>
2018-03-16 16:44:17 +0000

    Refactored alerts into help method GOOD

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,4 +72,22 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+  
+  
+  
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Qbecode Portfolio", sticky: false, time: 800)
+  end
+
+  
+  
+  
 end
